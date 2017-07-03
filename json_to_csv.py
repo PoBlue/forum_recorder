9c90d5047c308afdbaa3a1c96cb371ed0a93582f
blues <blues@blues.com>
2017-07-03 23:15:13 +0800

    version 1.0

--- json_to_csv.py
+++ json_to_csv.py
@@ -4,8 +4,27 @@ import json
 AND_FILE = "and.json"
 FEND_FILE = "fend.json"
 output_path = "output_csv/"
-with open(AND_FILE) as data_file:
-    data = json.load(data_file)
 
-table = rows.import_from_dicts(data)
-rows.export_to_csv(table, output_path + "and.csv")
+def get_record_data(data):
+    record_data = []
+    for elem in data:
+        isfounded = False
+        for record in record_data:
+            if record['title'] == elem['title']:
+                record['num'] += 1
+                isfounded = True
+        if isfounded == False:
+            elem['num'] = 1
+            record_data.append(elem)
+    return record_data
+
+def to_record_csv(json_file_path, output_name):
+    with open(json_file_path) as data_file:
+        data = json.load(data_file)
+
+    data = get_record_data(data)
+
+    data_table = rows.import_from_dicts(data)
+    rows.export_to_csv(data_table, output_path + output_name)
+
+to_record_csv(FEND_FILE, "fend.csv")
